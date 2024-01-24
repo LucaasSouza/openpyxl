bbe7bf002cf52e9c1620aa3eec1e36172d9e545b
LucasSouza <jadson.l.souza@ba.estudante.senai.br>
2024-01-24 13:46:24 -0300

    Adição de comentários no código

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from openpyxl import load_workbook
-from requests import get
 from tkinter import *
 from tkinter import messagebox
+from requests import get
+from openpyxl import load_workbook
 
 class Application:
     def __init__(self, master = None):
@@ -21,15 +21,20 @@ class Application:
         self.button.pack()
 
     def atualizarPlanilha(self):
-        path = self.input.get()
+        path = self.input.get() # Pega o que foi digitado pelo usuário
 
         try:
+            # Instância da planilha com base no caminho especificado
             planilha = load_workbook(path)
 
+            # Instância das células da planilha
+            celula = planilha.active
+
+            # GET dos dados que irão para a planilha
             lista_usuarios = get('https://jsonplaceholder.typicode.com/users')
             lista_usuarios = lista_usuarios.json()
 
-            celula = planilha.active
+            # Definição do que cada coluna irá apresentar. Key é o nome do objeto vindo da API 
             coords = [
                 { "col": "A", "key": "id" },
                 { "col": "B", "key": "name" },
@@ -38,17 +43,17 @@ class Application:
                 { "col": "E", "key": "phone" },
             ]
 
-            for c in coords:
-                for i in range(len(lista_usuarios)):
-                    celula[c['col'] + str(i + 1)].value = lista_usuarios[i][c['key']]
+            for c in coords: # Loop nas colunas/coordenadas
+                for i in range(len(lista_usuarios)): # Loop nos objetos/dict da API
+                    celula[c['col'] + str(i + 1)].value = lista_usuarios[i][c['key']] # Atualização do valor da célula [A1, A2, A3 ...]
 
-            planilha.save(path)
-            messagebox.showinfo(title='SUCESSO', message='Planilha atualizada com sucesso')
-            self.input.delete(0, 'end')
+            planilha.save(path) # Salva as alterações feitas na planilha
+            self.input.delete(0, 'end') # Limpa o input
+            messagebox.showinfo(title='SUCESSO', message='Planilha atualizada com sucesso') # Notificação de que a planilha foi alterada
         except:
-            messagebox.showerror(title='ERRO', message='Não foi possível executar a operação')
+            messagebox.showerror(title='ERRO', message='Não foi possível executar a operação') # Notificação de erro
 
-root = Tk()
+root = Tk() # Instância do módulo de interface gráfica
 root.title('Atualizar planilha')
 root.resizable(False, False)
 Application(root)
